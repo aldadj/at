75a5b7f3e70bbbe90d7948bbcabfe2462d516cbf
Aldadj Tech <alisnybelem@gmail.com>
2026-03-21 00:03:28 +0000

    Fix: View path in ChatController

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Force Laravel à faire confiance aux proxies de Render pour générer des URLs HTTPS
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
